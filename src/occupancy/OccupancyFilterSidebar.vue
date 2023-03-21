4000822b2d9ffeb2773d830b97f4d226c274ff8b
RichardOven <71844377+rvnlive@users.noreply.github.com>
2023-03-21 18:49:50 +0000

    Sidebar component

---
+++ src/occupancy/OccupancyFilterSidebar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card>
+    <v-list nav>
+      <v-list-item-group v-model="sidebarSelection" color="primary">
+        <v-list-item v-for="(floor, index) in floors" :key="index" :value="floor"> {{ floor }}</v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+const sidebarSelection = ref('All')
+
+const floors = computed(() => {
+  return ['Example', 'All'].sort()
+})
+</script>
