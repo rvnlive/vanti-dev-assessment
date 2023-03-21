37ef3bb5b53ab5eacb39136fb4c225feb95b7fac
RichardOven <71844377+rvnlive@users.noreply.github.com>
2023-03-21 20:40:16 +0000

    URL updated with query on option select

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,7 @@
   <v-app class="app-root">
     <v-app-bar app color="primary"> Vanti Front End Dev Assessment </v-app-bar>
     <v-main>
-      <v-container fluid class="d-flex flex-row">
-        <v-col cols="2" class="my-auto">
-          <occupancy-filter-sidebar />
-        </v-col>
-        <v-col cols="10">
-          <v-card>
-            <occupancy-table />
-          </v-card>
-        </v-col>
-      </v-container>
+      <router-view />
     </v-main>
   </v-app>
 </template>
-
-<script setup>
-import OccupancyTable from '@/occupancy/OccupancyTable.vue'
-import OccupancyFilterSidebar from '@/occupancy/OccupancyFilterSidebar.vue'
-</script>
