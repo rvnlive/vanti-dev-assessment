87a6b2c8425560cb808251ad2fbdf86a8f2528d5
RichardOven <71844377+rvnlive@users.noreply.github.com>
2023-03-21 18:53:30 +0000

    Display on page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <v-app class="app-root">
-    <v-app-bar app color="primary">
-      Vanti Front End Dev Assessment
-    </v-app-bar>
-
+    <v-app-bar app color="primary"> Vanti Front End Dev Assessment </v-app-bar>
     <v-main>
-      <v-container>
-        <v-card>
-          <occupancy-table/>
-        </v-card>
+      <v-container fluid class="d-flex flex-row">
+        <v-col cols="2" class="my-auto">
+          <occupancy-filter-sidebar />
+        </v-col>
+        <v-col cols="10">
+          <v-card>
+            <occupancy-table />
+          </v-card>
+        </v-col>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-import OccupancyTable from '@/occupancy/OccupancyTable.vue';
+import OccupancyTable from '@/occupancy/OccupancyTable.vue'
+import OccupancyFilterSidebar from '@/occupancy/OccupancyFilterSidebar.vue'
 </script>
